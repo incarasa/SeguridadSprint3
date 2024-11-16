604391e31b0df0a7f7833753e30ed758d20e3017
Raul Insuasty <r.insuasty@uniandes.edu.co>
2024-11-15 21:12:33 -0500

    cambio en views

--- manejador_usuarios/urls.py
+++ manejador_usuarios/urls.py
@@ -1,8 +1,8 @@
 # urls.py
 from django.urls import path
-from .views import EstudianteListView, PagoListView
+from . import views
 
 urlpatterns = [
-    path('estudiantes/', EstudianteListView.as_view(), name='estudiante-list'),
-    path('pagos/', PagoListView.as_view(), name='pago-list'),
+    path('estudiantes/', views.estudiante_list, name='estudiante_list'),
+    path('pagos/', views.pago_list, name='pago_list'),
 ]
--- manejador_usuarios/views.py
+++ manejador_usuarios/views.py
@@ -1,11 +1,18 @@
-from rest_framework import generics
+from django.http import JsonResponse
 from .models import Estudiante, Pago
-from .serializers import EstudianteSerializer, PagoSerializer
+# Descomentar cuando se cree el archivo ofiSecurity/auth0backend.py
+#from monitoring.auth0backend import getRole
 
-class EstudianteListView(generics.ListAPIView):
-    queryset = Estudiante.objects.all()
-    serializer_class = EstudianteSerializer
-
-class PagoListView(generics.ListAPIView):
-    queryset = Pago.objects.all()
-    serializer_class = PagoSerializer
+def estudiante_list(request):
+    # Ensure the request method is GET
+    if request.method == 'GET':
+        # Fetch data from the database (assuming a model named Estudiante)
+        estudiantes = Estudiante.objects.all().values()  # Get all records as a queryset of dictionaries
+        estudiantes_list = list(estudiantes)  # Convert queryset to list for JSON serialization
+        return JsonResponse(estudiantes_list, safe=False)  # Return JSON response
+    
+def pago_list(request):
+    if request.method == 'GET':
+        pagos = Pago.objects.all().values()
+        pagos_list = list(pagos)
+        return JsonResponse(pagos_list, safe=False)
